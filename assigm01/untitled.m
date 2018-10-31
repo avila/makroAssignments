fdea9432fa9bdc185a3a9d9d705eaa4fb2201f5d
Marcelo Avila <m.rainho.avila@gmail.com>
2018-11-01 00:08:26 +0100

    commit new version of assig 1

---
+++ assigm01/untitled.m
@@ -0,0 +1,6 @@
+%% Section 01
+% this comment is interpreted as normal text
+
+% this comment is interpreted as commented code 
+x = randn(100, 1);
+figure;plot(x);legend("x");title("output example")
